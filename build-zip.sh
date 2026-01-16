091889907d2f2264131a9e56decc5e193de0de92
DasterPL <daster7@o2.pl>
2026-01-16 09:34:42 +0100

    🔧Github workflow buld exclusions

--- build-zip.sh
+++ build-zip.sh
@@ -10,8 +10,12 @@ mkdir -p build
 rsync -av \
   --exclude '.git' \
   --exclude '.github' \
+  --exclude '.gitignore' \
   --exclude 'build' \
   --exclude '*.zip' \
+  --exclude 'build-zip.sh' \
+  --exclude 'composer.json' \
+  --exclude 'composer.lock' \
   --exclude 'node_modules' \
   ./ "build/${PLUGIN_SLUG}/"
 

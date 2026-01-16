41c11613b32bf43cd58b094783dddc93cb6dd0c7
DasterPL <daster7@o2.pl>
2026-01-16 09:48:22 +0100

    🔧Auto updater fix

--- ajax-snippets.php
+++ ajax-snippets.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: AJAX Snippets
 Description: Simple AJAX Snippets
-Version: 2.2.3
+Version: 2.2.4
 Requires at least: 4.9
 Tested up to: 6.9
 Requires PHP: 7.0
@@ -13,14 +13,12 @@ Author: Apturn
 
 defined('ABSPATH') || exit;
 
-define('AJAX_SNIPPETS_VERSION', '2.2.3');
+define('AJAX_SNIPPETS_VERSION', '2.2.4');
 define('AJAX_SNIPPETS_PLUGIN', __FILE__);
 define('AJAX_SNIPPETS_DIR', plugin_dir_path(AJAX_SNIPPETS_PLUGIN));
 define('AJAX_SNIPPETS_URL', plugin_dir_url(AJAX_SNIPPETS_PLUGIN));
 
-if (file_exists($autoload) && file_exists($puc_loader)) {
-    require_once AJAX_SNIPPETS_DIR . 'vendor/autoload.php';
-}
+require_once AJAX_SNIPPETS_DIR . 'vendor/autoload.php';
 
 require_once AJAX_SNIPPETS_DIR . 'includes/i18n.php';
 require_once AJAX_SNIPPETS_DIR . 'includes/updater.php';
--- includes/updater.php
+++ includes/updater.php
@@ -1,9 +1,8 @@
 <?php
-
 defined('ABSPATH') || exit;
 
-if (class_exists('Puc_v5_Factory')) {
-    $ajax_snippets_update_checker = Puc_v5_Factory::buildUpdateChecker(
+if (class_exists('YahnisElsts\PluginUpdateChecker\v5\PucFactory')) {
+    $ajax_snippets_update_checker = YahnisElsts\PluginUpdateChecker\v5\PucFactory::buildUpdateChecker(
         'https://github.com/DasterPL/ajax-snippets',
         AJAX_SNIPPETS_PLUGIN,
         'ajax-snippets'

e874c292e3a775c87412a735ebf2869675b07ae1
DasterPL <daster7@o2.pl>
2026-02-04 11:01:13 +0100

    🔧Auto updater fix

--- includes/updater.php
+++ includes/updater.php
@@ -1,11 +1,12 @@
 <?php
 defined('ABSPATH') || exit;
 
-if (class_exists('YahnisElsts\PluginUpdateChecker\v5\PucFactory')) {
-    $ajax_snippets_update_checker = YahnisElsts\PluginUpdateChecker\v5\PucFactory::buildUpdateChecker(
+if (class_exists('\YahnisElsts\PluginUpdateChecker\v5\PucFactory')) {
+    $ajax_snippets_update_checker = \YahnisElsts\PluginUpdateChecker\v5\PucFactory::buildUpdateChecker(
         'https://github.com/DasterPL/ajax-snippets',
         AJAX_SNIPPETS_PLUGIN,
         'ajax-snippets'
     );
     $ajax_snippets_update_checker->setBranch('main');
+    $ajax_snippets_update_checker->getVcsApi()->enableReleaseAssets('ajax-snippets.zip', \YahnisElsts\PluginUpdateChecker\v5p6\Vcs\Api::REQUIRE_RELEASE_ASSETS);
 }

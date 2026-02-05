ff7dd36c125d2c4afb46bc88a828c8cc1e24d651
DasterPL <daster7@o2.pl>
2026-02-05 21:15:23 +0100

    🔧Another updater fix

--- includes/updater.php
+++ includes/updater.php
@@ -1,8 +1,8 @@
 <?php
 defined('ABSPATH') || exit;
 
-if (class_exists('\YahnisElsts\PluginUpdateChecker\v5\PucFactory')) {
-    $ajax_snippets_update_checker = \YahnisElsts\PluginUpdateChecker\v5\PucFactory::buildUpdateChecker(
+if (class_exists('\YahnisElsts\PluginUpdateChecker\v5p6\PucFactory')) {
+    $ajax_snippets_update_checker = \YahnisElsts\PluginUpdateChecker\v5p6\PucFactory::buildUpdateChecker(
         'https://github.com/DasterPL/ajax-snippets',
         AJAX_SNIPPETS_PLUGIN,
         'ajax-snippets'
